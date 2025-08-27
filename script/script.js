55170746411b53f291b3aa45ed9512f08c27769a
davidmamoun <davidmamoun679@gmail.com>
2025-08-27 20:47:33 +0600

    added to history section

---
+++ script/script.js
@@ -0,0 +1,10 @@
+// document
+//   .getElementsByClassName("heart-icon")
+//   .addEventListener("click", function () {
+//     //   console.log("Heard icon clicked");
+//     const heartElement = document.getElementById("heart").innerText;
+//     console.log(heartElement);
+//   });
+
+const heartElement = document.getElementsByClassName("heart-icon");
+console.log(heartElement);
